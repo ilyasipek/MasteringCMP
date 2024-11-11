4519b410f5712e00525a5033648bbdfd212b9b8d
ilyasipek <ilyasipek1997@gmail.com>
2024-11-11 23:28:31 +0300

    refactor: format code

--- composeApp/src/iosMain/kotlin/com/mastering/cmp/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/mastering/cmp/Platform.ios.kt
@@ -2,7 +2,7 @@ package com.mastering.cmp
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
+class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
